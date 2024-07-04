60d8c6a0586fd999057a4df7cebbfc58b8726d23
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-07-03 20:20:55 -0500

    desafio completado

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 // Desafíos
-// Descarga otro proyecto haciendo clic en este enlace y ábrelo en Visual Studio Code.
-/*Cambia el contenido de la etiqueta h1 con document.querySelector y asigna el siguiente texto: "Hora del Desafío".*/
-/*Crea una función que muestre en la consola el mensaje "El botón fue clicado" siempre que se presione el botón "Console".*/
-/*Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil. Luego, muestra una alerta con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".*/
-/*Crea una función que muestre una alerta con el mensaje: "Yo amo JS" siempre que se presione el botón "Alerta".*/
-/*Al hacer clic en el botón "suma", pide 2 números y muestra el resultado de la suma en una alerta.*/
+/*1. Descarga otro proyecto haciendo clic en este enlace y ábrelo en Visual Studio Code.*/
+// Completado
+/*2. Cambia el contenido de la etiqueta h1 con document.querySelector y asigna el siguiente texto: "Hora del Desafío".*/
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora del Desafío';
+/*3. Crea una función que muestre en la consola el mensaje "El botón fue clicado" siempre que se presione el botón "Console".*/
+function mostrarMensaje() {
+    alert('El botón fue clicado');
+}
+/*4. Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil. Luego, muestra una alerta con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".*/
+function mostrarCiudad() {
+    let ciudad = prompt('¿Cuál es la ciudad de Brasil?');
+    alert(`Estuve en ${ciudad} y me acordé de ti`);
+}
+/*5. Crea una función que muestre una alerta con el mensaje: "Yo amo JS" siempre que se presione el botón "Alerta".*/
+function mostrarAlerta() {
+    alert('Yo amo JS');
+}
+/*6. Al hacer clic en el botón "suma", pide 2 números y muestra el resultado de la suma en una alerta.*/
+function suma() {
+    let numero1 = prompt('Ingrese el primer número');
+    let numero2 = prompt('Ingrese el segundo número');
+    let resultado = parseInt(numero1) + parseInt(numero2);
+    alert(`El resultado de la suma es ${resultado}`);
+}
